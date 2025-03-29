80e9bd3acae65b3f1c4f1552461baeb542cdc6aa
Gayatri Lande <gayatrilande15@gmail.com>
2025-03-29 13:14:29 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,14 @@
+class Mobile
+{
+	String brand;
+	String name;
+	String price;
+	String ram;
+	String memory;
+	String colour;
+	String camera;
+	String buildMaterial;
+	String battryLife;
+
+
+}
---
+++ MobileDriver.java
@@ -0,0 +1,104 @@
+class MobileDriver
+{
+	public static void main(String[] args) 
+	{
+		Mobile m1 = new Mobile();
+		Mobile m2 = new Mobile();
+		Mobile m3 = new Mobile();
+		Mobile m4 = new Mobile();
+
+
+		m1.brand= "Samsung";
+		m1.name = " galaxy m33";
+		m1.price= "20,000/-";
+		m1.ram = "8GB";
+	    m1.memory="128GB";
+	    m1.colour = "Blue";
+	    m1.camera = "32+12+16 mp";
+	    m1.buildMaterial = "fiber";
+	    m1.battryLife = "5000 mah";
+
+	    System.out.println("Brand : "+m1.brand);
+	    System.out.println("name: "+m1.name);
+	    System.out.println("price: "+m1.price);
+	    System.out.println("ram: "+m1.ram);
+        System.out.println("memory: "+m1.memory);
+	    System.out.println("colour: "+m1.colour);
+        System.out.println("camera: "+m1.camera);
+	    System.out.println("buildMaterial: "+m1.buildMaterial);
+	    System.out.println("battry: "+m1.battryLife);
+
+	    System.out.println("----------------------------------------------------------");
+
+	    m2.brand= "vivo";
+		m2.name = " vivo y20";
+		m2.price= "17,000/-";
+		m2.ram = "8GB";
+	    m2.memory="64GB";
+	    m2.colour = "purple";
+	    m2.camera = "32+20+8 mp";
+	    m2.buildMaterial = "fiber";
+	    m2.battryLife = "5500 mah";
+
+	    System.out.println("Brand : "+m2.brand);
+	    System.out.println("name: "+m2.name);
+	    System.out.println("price: "+m2.price);
+	    System.out.println("ram: "+m2.ram);
+        System.out.println("memory: "+m2.memory);
+	    System.out.println("colour: "+m2.colour);
+        System.out.println("camera: "+m2.camera);
+	    System.out.println("buildMaterial: "+m2.buildMaterial);
+	    System.out.println("battry: "+m2.battryLife);
+
+	    System.out.println("------------------------------------------------------------------");
+
+
+	    m3.brand= "oneplus";
+		m3.name = "onenord ";
+		m3.price= "25,000/-";
+		m3.ram = "24GB";
+	    m3.memory="528";
+	    m3.colour = "white";
+	    m3.camera = "92+60+8 mp";
+	    m3.buildMaterial = "plastic";
+	    m3.battryLife = "7000 mah";
+
+	    System.out.println("Brand : "+m3.brand);
+	    System.out.println("name: "+m3.name);
+	    System.out.println("price: "+m3.price);
+	    System.out.println("ram: "+m3.ram);
+        System.out.println("memory: "+m3.memory);
+	    System.out.println("colour: "+m3.colour);
+        System.out.println("camera: "+m3.camera);
+	    System.out.println("buildMaterial: "+m3.buildMaterial);
+	    System.out.println("battry: "+m3.battryLife);
+
+	    System.out.println("------------------------------------------------------------------");
+
+	    m4.brand= "realmi";
+		m4.name = " realmi w23";
+		m4.price= "10,000/-";
+		m4.ram = "4BG";
+	    m4.memory="64GB";
+	    m4.colour = "black";
+	    m4.camera = "12+12+8 mp";
+	    m4.buildMaterial = "fiber";
+	    m4.battryLife = "4000 mah";
+
+	    System.out.println("Brand : "+m4.brand);
+	    System.out.println("name: "+m4.name);
+	    System.out.println("price: "+m4.price);
+	    System.out.println("ram: "+m4.ram);
+        System.out.println("memory: "+m4.memory);
+	    System.out.println("colour: "+m4.colour);
+        System.out.println("camera: "+m4.camera);
+	    System.out.println("buildMaterial: "+m4.buildMaterial);
+	    System.out.println("battry: "+m4.battryLife);
+
+	    System.out.println("------------------------------------------------------------------");
+
+
+
+		
+	}
+}
