d9f024b90b580025b5818125e004792bbaca6bac
Gayatri Lande <gayatrilande15@gmail.com>
2025-04-07 00:26:21 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,19 @@
+class Bike
+{
+	String brand;
+	String name;
+	double price;
+	int engine;
+	int mileage;
+
+	public void displayBike()
+	{
+		String name ="jupiter zx";
+		System.out.println(brand);
+		System.out.println(name);
+		System.out.println(this.name);
+		System.out.println(price);
+		System.out.println(engine);
+		System.out.println(mileage);
+	}
+}
